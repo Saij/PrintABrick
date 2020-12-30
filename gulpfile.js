11a731fb5ee86eb76e41f1ab8bb4e7bc242f938a
Saij <christoph@christophfriedrich.de>
2020-12-30 10:42:09 +0100

    Updating Gulp to newest version

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'),
     plugins = require('gulp-load-plugins')();
 
-gulp.task('css', function() {
+gulp.task('css', gulp.series(() => {
     return gulp.src([
         'bower_components/semantic/dist/semantic.css',
         'bower_components/lightbox2/dist/css/lightbox.css',
@@ -9,33 +9,31 @@ gulp.task('css', function() {
         'bower_components/jQuery-ui-Slider-Pips/dist/jquery-ui-slider-pips.css',
         'bower_components/select2/dist/css/select2.css',
         'app/Resources/assets/style/style.scss',
-    ])
-        .pipe(plugins.sass().on('error', plugins.sass.logError))
+    ]).pipe(plugins.sass().on('error', plugins.sass.logError))
         .pipe(plugins.concat('style.css', {newLine: ' '}))
         .pipe(plugins.environments.production(plugins.cssmin()))
         .pipe(gulp.dest('web/resources/css'));
-});
+}));
 
-gulp.task('three', function() {
+gulp.task('three', gulp.series((done) => {
     gulp.src([
         'bower_components/three.js/build/three.js',
         'bower_components/three.js/examples/js/libs/stats.min.js',
-        'bower_components/three.js/examples/js/loaders/STLLoader.js',
-        'bower_components/three.js/examples/js/Detector.js',
-    ])
-        .pipe(plugins.concat('three.js'))
+        'bower_components/three.js/examples/js/loaders/STLLoader.js'
+    ]).pipe(plugins.concat('three.js'))
         .pipe(plugins.environments.production(plugins.uglify()))
         .pipe(gulp.dest('web/resources/js'));
 
     gulp.src([
         'bower_components/three.js/examples/js/controls/OrbitControls.js',
-    ])
-        .pipe(plugins.concat('OrbitControls.js'))
+    ]).pipe(plugins.concat('OrbitControls.js'))
         .pipe(plugins.environments.production(plugins.uglify()))
         .pipe(gulp.dest('web/resources/js'));
-});
 
-gulp.task('js', function() {
+    return done();
+}));
+
+gulp.task('js', gulp.series(() => {
     return gulp.src([
         'bower_components/jquery/dist/jquery.js',
         'bower_components/jquery-ui/jquery-ui.js',
@@ -46,32 +44,30 @@ gulp.task('js', function() {
         'bower_components/semantic/dist/semantic.js',
         'bower_components/lightbox2/dist/js/lightbox.js',
         'app/Resources/assets/js/**.js',
-    ])
-        .pipe(plugins.concat('style.js'))
+    ]).pipe(plugins.concat('style.js'))
         .pipe(plugins.environments.production(plugins.uglify()))
         .pipe(gulp.dest('web/resources/js'));
-});
+}));
 
-gulp.task('files:semantic', function () {
+gulp.task('files:semantic', gulp.series(() => {
     return gulp.src(
         'bower_components/semantic/dist/themes/**'
-    )
-        .pipe(gulp.dest('web/resources/css/themes'));
-});
+    ).pipe(gulp.dest('web/resources/css/themes'));
+}));
 
-gulp.task('files:images', function () {
+gulp.task('files:images', gulp.series(() => {
     return gulp.src([
         'bower_components/lightbox2/dist/images/**',
         'app/Resources/assets/images/**'
-    ])
-        .pipe(gulp.dest('web/resources/images'));
-});
+    ]).pipe(gulp.dest('web/resources/images'));
+}));
 
-gulp.task('watch', ['js', 'css', 'three'], function () {
+gulp.task('watch', gulp.series(['js', 'css', 'three'], (done) => {
     gulp.watch('app/Resources/assets/js/**.js' , ['js']);
     gulp.watch('app/Resources/assets/style/**/*.scss' , ['css']);
-});
+    return done();
+}));
 
-gulp.task('default', function () {
-    return gulp.start(['files:semantic', 'files:images', 'js', 'css', 'three']);
-});
+gulp.task('default', gulp.series(['files:semantic', 'files:images', 'js', 'css', 'three'], (done) => {
+    return done();
+}));
